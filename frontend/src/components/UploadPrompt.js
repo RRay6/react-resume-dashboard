d1beb1e18a3da76dc8feda1c8a8105ed98eadd28
RRay6 <rray6@gmu.edu>
2022-08-13 07:13:41 -0400

    Backend Changes
    Finished backend implementation. Hopefully.

---
+++ frontend/src/components/UploadPrompt.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function UploadPrompt({ file }) {
+  if (!file) {
+    return (
+      <div className="uploadPromptDefault">
+        <span>
+          drag and drop or <u>browse</u>{' '}
+        </span>
+      </div>
+    );
+  } else if (file.length > 1) {
+    return (
+      <div className="uploadPromptSelected">{file.length} files selected</div>
+    );
+  } else {
+    return <div className="uploadPromptSelected">{file[0].name}</div>;
+  }
+}
---
+++ frontend/src/components/databaseUpload.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { db } from './config/firebaseConfig';
+import { set, ref, onValue, remove, update } from 'firebase/database';
+import { uid } from 'uid';
+import Upload2firebase from './upload2Firebase';
+
+// Parsed file keywords will be written to a file
+// New file will then be written firebase Realtime database
+// It will then display on the webApp
+// We can upload to firebase for storage and at the same time parse the file
+// and create the new file to write to the DB
+const FirebaseDB = () => {
+  const [name, setName] = useState('');
+  const [names, setNames] = useState([]);
+  const [email, setEmail] = useState('');
+  const [phoneNum, setPhoneNum] = useState('');
+  const [location, setLocation] = useState('');
+  const [company, setCompany] = useState('');
+  const [skills, setSkills] = useState('');
+
+  //read
+  useEffect(() => {
+    onValue(ref(db, `/resumes`), (snapshot) => {
+      setNames([]);
+      const data = snapshot.val();
+      if (data !== null) {
+        Object.values(data).forEach((name) => {
+          setNames((oldArray) => [...oldArray, name]);
+        });
+      }
+    });
+  }, []);
+  //write
+  const write2db = () => {
+    const uuid = uid();
+    set(ref(db, `/resumes/${uuid}`), {
+      uuid,
+      name,
+      email,
+      phoneNum,
+      location,
+      company,
+      skills,
+    });
+    setName('');
+    setEmail('');
+    setPhoneNum('');
+    setLocation('');
+    setCompany('');
+    setSkills('');
+  };
+
+  // names.maps maps info according to name, will probably be on
+  // on resumeTile.js file
+  return (
+    <div className="uploadNameContainer">
+    <span>Name</span>
+    <input className="uploadNameForm" input="text" placeholder="enter name of applicant"></input>
+    
+    <table>
+      {names.map((name) => (
+        <>
+          <tr key={name.name}></tr>
+          <th>{name.name}</th>
+          <td> {name.email}</td>
+          <td>{name.phoneNum}</td>
+          <td>{name.location} </td>
+          <td>{name.skills} </td>
+        </>
+      ))}
+      
+    </table>
+    </div>
+  );
+};
+
+export default FirebaseDB;
