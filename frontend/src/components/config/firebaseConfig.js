46c873e1859a579f5f4000496d4ce2a9bd663994
Glorentino <86209684+Glorentino@users.noreply.github.com>
2022-08-24 19:32:19 -0400

    Delete Api Key

--- frontend/src/components/config/firebaseConfig.js
+++ frontend/src/components/config/firebaseConfig.js
@@ -3,12 +3,7 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 // const firebaseConfig = {
-//     apiKey: "AIzaSyApcc3ArqnoFjz9-xyAVBfYjUK1crvdA-Q",
-//     authDomain: "resumeuploaderstorage.firebaseapp.com",
-//     projectId: "resumeuploaderstorage",
-//     storageBucket: "resumeuploaderstorage.appspot.com",
-//     messagingSenderId: "626364308531",
-//     appId: "1:626364308531:web:e516d4069cc1af269bd746"};
+};
 // // Firebase Api Key inputed into the bracket
 
 // const app = initializeApp(firebaseConfig);
